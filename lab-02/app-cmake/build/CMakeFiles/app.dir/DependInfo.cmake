02b3dd0e377595a00c844bd67d4537d779b7e50f
Sasa Mikic <sasa.mikic@student.etf.unibl.org>
2024-03-20 13:00:31 +0100

    cmake

---
+++ lab-02/app-cmake/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lesam/urs-2024/lab-02/app-cmake/app.c" "CMakeFiles/app.dir/app.c.o" "gcc" "CMakeFiles/app.dir/app.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lesam/urs-2024/lab-02/app-cmake/build/test/CMakeFiles/test.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab-02/app-cmake/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/app.c.o"
+  "CMakeFiles/app.dir/app.c.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
