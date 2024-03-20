5a378181934fd876b0ec4cab699ce1700352cc16
Mladen Knezic <mladen.knezic@etf.unibl.org>
2024-03-20 02:13:34 +0100

    Added example toolchain file for CMake

---
+++ lab-02/toolchains/arm_cortex_a9.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_SYSROOT /home/mknezic/x-tools/arm-etfbl-linux-gnueabihf/arm-etfbl-linux-gnueabihf/sysroot)
+set(CMAKE_STAGING_PREFIX ./build)
+
+set(tools /home/mknezic/x-tools/arm-etfbl-linux-gnueabihf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gcc)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
