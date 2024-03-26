efcf8fcc4ed8c62adac66462436266aa4221b580
Ubuntu <nemanja.kopanja18@yahoo.com>
2024-03-26 05:49:08 -0700

    Lab 2 commit

---
+++ lab-02/app-cmake/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nemanja/urs-2024/lab-02/app-cmake/app.c" "/home/nemanja/urs-2024/lab-02/app-cmake/build/CMakeFiles/app.dir/app.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nemanja/urs-2024/lab-02/app-cmake/build/test/CMakeFiles/test.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab-02/app-cmake/build/test/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/print.c.o"
+  "CMakeFiles/test.dir/sum.c.o"
+  "libtest.a"
+  "libtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
