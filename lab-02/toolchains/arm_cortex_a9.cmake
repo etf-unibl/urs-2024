1df24c36002730aa289ace2b542a05e52eab7fd9
Sasa Mikic <sasa.mikic@student.etf.unibl.org>
2024-03-26 11:55:15 +0100

    delete

--- lab-02/toolchains/arm_cortex_a9.cmake
+++ lab-02/toolchains/arm_cortex_a9.cmake
@@ -1,11 +1,13 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_SYSROOT /home/mknezic/x-tools/arm-etfbl-linux-gnueabihf/arm-etfbl-linux-gnueabihf/sysroot)
+set(CMAKE_SYSROOT /home/lesam/x-tools/arm-etfbl-linux-gnueabihf/arm-etfbl-linux-gnueabihf/sysroot)
 set(CMAKE_STAGING_PREFIX ./build)
 
-set(tools /home/mknezic/x-tools/arm-etfbl-linux-gnueabihf)
+set(tools /home/lesam/x-tools/arm-etfbl-linux-gnueabihf)
 set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gcc)
+
+set(CMAKE_PREFIX_PATH ${CMAKE_SYSROOT}/usr/lib)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
